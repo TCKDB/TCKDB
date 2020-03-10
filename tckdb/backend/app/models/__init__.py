9eb0e3233dd2b31abea2f5544a960c58ed076dcb
Alon Grinberg Dana <alongd@mit.edu>
2020-03-10 14:34:03 -0400

    Added an Author model

---
+++ tckdb/backend/app/models/__init__.py
@@ -0,0 +1,6 @@
+"""
+Initialize the TCKDB backend app models module for SQLAlchemy models
+
+The SQLAlchemy Object Relational Mapper (ORM) presents a method of associating user-defined Python classes
+with database tables, and instances of those classes (objects) with rows in their corresponding tables.
+"""
