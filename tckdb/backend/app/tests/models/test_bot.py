c1f24ca4a413ab92f0c1f5f44842b5eb52734e25
Alon Grinberg Dana <alongd@mit.edu>
2020-03-10 21:20:22 -0400

    Tests: Bot model

---
+++ tckdb/backend/app/tests/models/test_bot.py
@@ -0,0 +1,23 @@
+"""
+TCKDB backend app tests models test_bot module
+"""
+
+import unittest
+
+from tckdb.backend.app.models.bot import Bot
+
+
+class TestBotModel(unittest.TestCase):
+    """
+    Contains unit tests for the Author module
+    """
+
+    def test_bot_model(self):
+        """Test creating an instance of Author"""
+        bot_1 = Bot(name='ARC', version='1.1.0', url='https://github.com/ReactionMechanismGenerator/ARC')
+        self.assertEqual(bot_1.name, 'ARC')
+        self.assertEqual(bot_1.version, '1.1.0')
+        self.assertEqual(bot_1.url, 'https://github.com/ReactionMechanismGenerator/ARC')
+        self.assertEqual((str(bot_1)),
+                         '<Bot(id="None", name="ARC", version="1.1.0", '
+                         'url="https://github.com/ReactionMechanismGenerator/ARC")>')
