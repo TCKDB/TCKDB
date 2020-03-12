184b09e18850ddefa250ccc8a50c93aa30534674
Alon Grinberg Dana <alongd@mit.edu>
2020-03-11 21:47:05 -0400

    Tests: schemas common lowercase_dict

---
+++ tckdb/backend/app/tests/schemas/test_common.py
@@ -0,0 +1,24 @@
+"""
+TCKDB backend app tests schemas test_freq module
+"""
+
+import unittest
+
+from tckdb.backend.app.schemas.common import lowercase_dict
+
+
+class TestCommon(unittest.TestCase):
+    """
+    Contains unit tests for the schemas common module
+    """
+
+    def test_lowercase_dict(self):
+        """Test attaining a dictionary with lowercase keys and values"""
+        d_1 = {'D': 1}
+        self.assertEqual(lowercase_dict(d_1), {'d': 1})
+        d_2 = {'D': 'Z'}
+        self.assertEqual(lowercase_dict(d_2), {'d': 'z'})
+        d_3 = {5: 'Z'}
+        self.assertEqual(lowercase_dict(d_3), {5: 'z'})
+        d_4 = {'D': {'A': 'Z', 'v': 'n', 'H': 54, 'f': 'L', 7: 0, 8: {'Q': 9}}}
+        self.assertEqual(lowercase_dict(d_4), {'d': {'a': 'z', 'v': 'n', 'h': 54, 'f': 'l', 7: 0, 8: {'q': 9}}})
