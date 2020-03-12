abd4ce8b35abc20f07b35ef1ab0df0e52e06a680
Alon Grinberg Dana <alongd@mit.edu>
2020-03-11 20:37:49 -0400

    Minor: Style modifications in models
    - Modified string quotation marks to comply with the dictionary representation in the database, so writing tests is easier
    - Added Attributes to models docstring
    - Added return type hints

--- tckdb/backend/app/models/author.py
+++ tckdb/backend/app/models/author.py
@@ -10,16 +10,22 @@ from tckdb.backend.app.db.base_class import Base
 class Author(Base):
     """
     A class for representing a TCKDB Author
+
+    Attributes:
+        id (int): The primary key.
+        name (str): The Author's full name.
+        email (str): The Author's email address.
+        affiliation (str): The Author's academic affiliation.
     """
     id = Column(Integer, primary_key=True, index=True, nullable=False)
     name = Column(String(255), unique=True, nullable=False)
     email = Column(String(255), unique=True, nullable=False)
     affiliation = Column(String(255), nullable=False)
 
-    def __repr__(self):
-        return f'<{self.__class__.__name__}(' \
-               f'id="{self.id}", ' \
-               f'name="{self.name}", ' \
-               f'email="{self.email}", ' \
-               f'affiliation="{self.affiliation}"' \
-               f')>'
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__}(" \
+               f"id={self.id}, " \
+               f"name='{self.name}', " \
+               f"email='{self.email}', " \
+               f"affiliation='{self.affiliation}'" \
+               f")>"
