ea8974bc3e7d41f4148d52388dd8b8acd1e981c5
Alon Grinberg Dana <alongd@mit.edu>
2020-05-11 16:51:18 -0400

    Modified JL model and schema

--- tckdb/backend/app/models/LJ.py
+++ tckdb/backend/app/models/LJ.py
@@ -8,6 +8,7 @@ from sqlalchemy import Column, Integer
 from sqlalchemy.dialects.postgresql import ARRAY
 
 from tckdb.backend.app.db.base_class import Base
+from tckdb.backend.app.models.common import JSONEncodedDict, MutableDict
 
 
 class LJ(Base):
@@ -18,12 +19,17 @@ class LJ(Base):
         id (int): The primary key.
         sigma (tuple): The L-J sigma parameter.
         epsilon (tuple): The L-J epsilon parameter.
+        reviewer_flags (dict): Backend flags to assist the review process.
     """
     id = Column(Integer, primary_key=True, index=True, nullable=False)
     sigma = Column(ARRAY(item_type=Union[float, str], as_tuple=True, dimensions=2, zero_indexes=True), nullable=False)
     epsilon = Column(ARRAY(item_type=Union[float, str], as_tuple=True, dimensions=2, zero_indexes=True), nullable=False)
+    reviewer_flags = Column(MutableDict.as_mutable(JSONEncodedDict), nullable=True)
 
     def __repr__(self) -> str:
+        """
+        A string representation from which the object can be reconstructed.
+        """
         return f"<{self.__class__.__name__}(" \
                f"id={self.id}, " \
                f"sigma={self.sigma}, " \
