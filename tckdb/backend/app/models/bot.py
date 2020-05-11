bd5e6019256ea238a59da406dd8d990d428e8b64
Alon Grinberg Dana <alongd@mit.edu>
2020-05-11 16:48:48 -0400

    Modified Bot model and schema

--- tckdb/backend/app/models/bot.py
+++ tckdb/backend/app/models/bot.py
@@ -5,6 +5,7 @@ TCKDB backend app models bot module
 from sqlalchemy import Column, Integer, String
 
 from tckdb.backend.app.db.base_class import Base
+from tckdb.backend.app.models.common import JSONEncodedDict, MutableDict
 
 
 class Bot(Base):
@@ -17,16 +18,37 @@ class Bot(Base):
         name (str): The software name.
         version (str): The software version.
         url (str): The official software web address.
+        git_commit (str): The git commit hash used for this run.
+        git_branch (str): The git branch used for this run.
+        reviewer_flags (dict): Backend flags to assist the review process.
     """
     id = Column(Integer, primary_key=True, index=True, nullable=False)
     name = Column(String(100), unique=True, nullable=False)
-    version = Column(String(100))
+    version = Column(String(100), nullable=False)
     url = Column(String(255), nullable=False)
+    git_commit = Column(String(500), nullable=True)
+    git_branch = Column(String(100), nullable=True)
+    reviewer_flags = Column(MutableDict.as_mutable(JSONEncodedDict), nullable=True)
+
+    def __str__(self) -> str:
+        """
+        A string representation from which the object can be reconstructed.
+        """
+        return f"<{self.__class__.__name__}(" \
+               f"name='{self.name}', " \
+               f"version='{self.version}', " \
+               f"url='{self.url}'" \
+               f")>"
 
     def __repr__(self) -> str:
+        """
+        A string representation from which the object can be reconstructed.
+        """
         return f"<{self.__class__.__name__}(" \
                f"id={self.id}, " \
                f"name='{self.name}', " \
                f"version='{self.version}', " \
-               f"url='{self.url}'" \
+               f"url='{self.url}', " \
+               f"git_commit='{self.git_commit}', " \
+               f"git_branch='{self.git_branch}'" \
                f")>"
--- tckdb/backend/app/schemas/bot.py
+++ tckdb/backend/app/schemas/bot.py
@@ -2,6 +2,8 @@
 TCKDB backend app schemas bot module
 """
 
+from typing import Dict, Optional
+
 from pydantic import BaseModel, constr, validator
 
 
@@ -10,8 +12,16 @@ class BotBase(BaseModel):
     A BotBase class (shared properties)
     """
     name: constr(max_length=100)
-    version: constr(max_length=100) = None
+    version: Optional[constr(max_length=100)] = None
     url: constr(max_length=255)
+    git_commit: Optional[constr(max_length=100)] = None
+    git_branch: Optional[constr(max_length=100)] = None
+    reviewer_flags: Optional[Dict[str, str]] = None
+
+    @validator('reviewer_flags', always=True)
+    def check_reviewer_flags(cls, value):
+        """Bot.reviewer_flags validator"""
+        return value or dict()
 
     @validator('url')
     def validate_url(cls, value):
@@ -22,27 +32,43 @@ class BotBase(BaseModel):
             raise ValueError('url invalid (no spaces allowed)')
         return value
 
+    @validator('git_commit')
+    def validate_git_commit(cls, value):
+        """Bot.git_commit validator"""
+        if not value.isalnum():
+            raise ValueError(f'The git commit seems wrong, got {value}')
+        return value
+
 
 class BotCreate(BotBase):
     """Create a Bot item: Properties to receive on item creation"""
     name: str
-    version: str = None
+    version: Optional[str] = None
     url: str
+    git_commit: Optional[str] = None
+    git_branch: Optional[str] = None
+    reviewer_flags: Optional[Dict[str, str]] = None
 
 
 class BotUpdate(BotBase):
     """Update a Bot item: Properties to receive on item update"""
     name: str
-    version: str
+    version: Optional[str] = None
     url: str
+    git_commit: Optional[str] = None
+    git_branch: Optional[str] = None
+    reviewer_flags: Optional[Dict[str, str]] = None
 
 
 class BotInDBBase(BotBase):
     """Properties shared by models stored in DB"""
     id: int
     name: str
-    version: str
+    version: Optional[str] = None
     url: int
+    git_commit: Optional[str] = None
+    git_branch: Optional[str] = None
+    reviewer_flags: Optional[Dict[str, str]] = None
 
     class Config:
         orm_mode = True
