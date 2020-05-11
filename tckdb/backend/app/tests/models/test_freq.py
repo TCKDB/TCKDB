8f2908761427cc38bbb9ca623dfabb0e479fc66b
Alon Grinberg Dana <alongd@mit.edu>
2020-05-11 16:49:47 -0400

    Tests: Modified Freq model and schema tests

--- tckdb/backend/app/tests/models/test_freq.py
+++ tckdb/backend/app/tests/models/test_freq.py
@@ -1,40 +1,26 @@
 """
-TCKDB backend app tests models test_author module
+TCKDB backend app tests models test_freq module
 """
 
 from tckdb.backend.app.models.freq import Freq
 
 
-def test_author_model():
-    """Test creating an instance of Author"""
-    freq1 = Freq(level={'method': 'cbs-qb3'}, factor=0.99 * 1.014,
+def test_freq_model():
+    """Test creating an instance of Freq"""
+    freq1 = Freq(factor=0.99 * 1.014,
                  source='J.A. Montgomery, M.J. Frisch, J. Chem. Phys. 1999, 110, 2822–2827, DOI: 10.1063/1.477924')
-    assert freq1.level == {'method': 'cbs-qb3'}
     assert freq1.factor == 0.99 * 1.014
     assert freq1.source == 'J.A. Montgomery, M.J. Frisch, J. Chem. Phys. 1999, 110, 2822–2827, DOI: 10.1063/1.477924'
-    assert repr(freq1) == "<Freq(id=None, " \
-                          "level={'method': 'cbs-qb3'}, " \
-                          "factor=1.00386, " \
-                          "source='J.A. Montgomery, M.J. Frisch, J. Chem. Phys. 1999, 110, 2822–2827, " \
-                          "DOI: 10.1063/1.477924')>"
-    assert str(freq1) == "<Freq(level={'method': 'cbs-qb3'}, factor=1.00386, " \
-                         "source='J.A. Montgomery, M.J. Frisch, J. Chem. Phys. 1999, 110, 2822–2827, " \
-                         "DOI: 10.1063/1.477924')>"
+    assert repr(freq1) == "<Freq(id=None, factor=1.00386, level_id=None, source='J.A. Montgomery, M.J. Frisch, " \
+                          "J. Chem. Phys. 1999, 110, 2822–2827, DOI: 10.1063/1.477924')>"
+    assert str(freq1) == "<Freq(factor=1.00386, level_id=None, source='J.A. Montgomery, " \
+                         "M.J. Frisch, J. Chem. Phys. 1999, 110, 2822–2827, DOI: 10.1063/1.477924')>"
 
-    freq2 = Freq(level={'method': 'wB97xd', 'basis': 'def2TZVP', 'solvation': {'method': 'PCM', 'solvent': 'water'}},
-                 factor=0.98, source="Calculated using the Truhlar method")
-    assert str(freq2) == "<Freq(level={'method': 'wB97xd', 'basis': 'def2TZVP', 'solvation': " \
-                         "{'method': 'PCM', 'solvent': 'water'}}, factor=0.98, " \
-                         "source='Calculated using the Truhlar method')>"
+    freq2 = Freq(factor=0.98, source='Calculated using the Truhlar method')
+    assert str(freq2) == "<Freq(factor=0.98, level_id=None, source='Calculated using the Truhlar method')>"
 
-    freq3 = Freq(level={'method': 'B3LYP', 'basis': '6-31G(d,p)', 'dispersion': 'gd3bj'},
-                 factor=0.98, source="Calculated using the Truhlar method")
-    assert str(freq3) == "<Freq(level={'method': 'B3LYP', 'basis': '6-31G(d,p)', 'dispersion': 'gd3bj'}, factor=0.98," \
-                         " source='Calculated using the Truhlar method')>"
+    freq3 = Freq(factor=0.98, source='Calculated using the Truhlar method')
+    assert str(freq3) == "<Freq(factor=0.98, level_id=None, source='Calculated using the Truhlar method')>"
 
-    freq4 = Freq(level={'method': 'DLPNO-CCSD(T)-F12', 'basis': 'cc-pVTZ-F12',
-                        'auxiliary_basis': 'aug-cc-pVTZ/C cc-pVTZ-F12-CABS'},
-                 factor=0.98, source="Calculated using the Truhlar method")
-    assert str(freq4) == "<Freq(level={'method': 'DLPNO-CCSD(T)-F12', 'basis': 'cc-pVTZ-F12', " \
-                         "'auxiliary_basis': 'aug-cc-pVTZ/C cc-pVTZ-F12-CABS'}, factor=0.98, " \
-                         "source='Calculated using the Truhlar method')>"
+    freq4 = Freq(factor=0.98, source='Calculated using the Truhlar method')
+    assert str(freq4) == "<Freq(factor=0.98, level_id=None, source='Calculated using the Truhlar method')>"
--- tckdb/backend/app/tests/schemas/test_freq.py
+++ tckdb/backend/app/tests/schemas/test_freq.py
@@ -10,48 +10,30 @@ from tckdb.backend.app.schemas.freq import FreqBase
 
 def test_freq_schema():
     """Test creating an instance of Literature"""
-    freq_1 = FreqBase(level={'method': 'CBS-QB3'},
-                      factor=0.99 * 1.014,
+    freq_1 = FreqBase(factor=0.99 * 1.014, level_id=1,
                       source='J.A. Montgomery, M.J. Frisch, J. Chem. Phys. 1999, 110, 2822–2827, '
                              'DOI: 10.1063/1.477924'
                       )
-    assert freq_1.level == {'method': 'cbs-qb3'}
+    assert freq_1.factor == 1.00386
 
-    freq_2 = FreqBase(level={'method': 'wB97xd', 'basis': 'def2TZVP',
-                             'solVation': {'Method': 'PCM', 'solvent': 'Water'}},
-                      factor=0.97,
+    freq_2 = FreqBase(factor=0.97, level_id=1,
                       source='Calculated using the Truhlar method'
                       )
-    assert freq_2.level == {'method': 'wb97xd', 'basis': 'def2tzvp',
-                            'solvation': {'method': 'pcm', 'solvent': 'water'}}
     assert freq_2.factor == 0.97
     assert freq_2.source == 'Calculated using the Truhlar method'
 
     with pytest.raises(ValidationError):
-        # no level
-        FreqBase(factor=0.95, source='Calculated using the Truhlar method')
-    with pytest.raises(ValidationError):
         # no factor
-        FreqBase(level={'method': 'cbs-qb3'}, source='Calculated using the Truhlar method')
+        FreqBase(level_id=1, source='Calculated using the Truhlar method')
+    with pytest.raises(ValidationError):
+        # no level_id
+        FreqBase(factor=1.01, source='Calculated using the Truhlar method')
     with pytest.raises(ValidationError):
         # no source
-        FreqBase(level={'method': 'cbs-qb3'}, factor=0.95)
+        FreqBase(factor=0.95, level_id=1)
     with pytest.raises(ValidationError):
-        # no method in level
-        FreqBase(level={'basis': 'def2TZVP'}, factor=0.95)
+        # negative factor
+        FreqBase(factor=-0.95, level_id=1)
     with pytest.raises(ValidationError):
-        # wrong solvation type
-        FreqBase(level={'method': 'b3lyp', 'basis': 'def2TZVP', 'solvation': 'pcm'}, factor=0.95)
-    with pytest.raises(ValidationError):
-        # no method in solvation
-        FreqBase(level={'method': 'b3lyp', 'basis': 'def2TZVP', 'solvation': {'solvent': 'water'}}, factor=0.95)
-    with pytest.raises(ValidationError):
-        # no solvent in solvation
-        FreqBase(level={'method': 'b3lyp', 'basis': 'def2TZVP', 'solvation': {'method': 'pcm'}}, factor=0.95)
-    with pytest.raises(ValidationError):
-        # illegal key in solvation
-        FreqBase(level={'method': 'b3lyp', 'basis': 'def2TZVP', 'solvation':
-            {'method': 'pcm', 'solvent': 'water', 'illegal key': 'value'}}, factor=0.95)
-    with pytest.raises(ValidationError):
-        # illegal key in level
-        FreqBase(level={'method': 'b3lyp', 'basis': 'def2TZVP', 'illegal key': 'value'}, factor=0.95)
+        # large factor
+        FreqBase(factor=95, level_id=1)
