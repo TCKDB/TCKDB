1813d939c5b8630359fce83d2114db8702121e6a
Alon Grinberg Dana <alongd@mit.edu>
2020-05-11 16:52:43 -0400

    Tests: Modified Trans model and schema tests

--- tckdb/backend/app/tests/schemas/test_trans.py
+++ tckdb/backend/app/tests/schemas/test_trans.py
@@ -18,47 +18,63 @@ def test_trans_model_enum():
 def test_trans_schema():
     """Test creating an instance of Trans"""
     trans_1 = TransBase(model='Single Exponential Down',
-                        parameters={'alpha0': (175, 'cm^-1'), 'T0': (300, 'K'), 'n': (0.52,)},
-                        )
+                        parameters={'alpha0': (175, 'cm^-1'), 'T0': (300, 'K'), 'n': 0.52})
     assert trans_1.model == 'Single Exponential Down'
-    assert trans_1.parameters == {'alpha0': (175, 'cm^-1'), 'T0': (300, 'K'), 'n': (0.52,)}
+    assert trans_1.parameters == {'alpha0': (175, 'cm^-1'), 'T0': (300, 'K'), 'n': 0.52}
 
     with pytest.raises(ValidationError):
         # wrong model
-        TransBase(model='wrong', parameters={'alpha0': (175, 'cm^-1'), 'T0': (300, 'K'), 'n': (0.52,)})
+        TransBase(model='wrong',
+                  parameters={'alpha0': (175, 'cm^-1'), 'T0': (300, 'K'), 'n': 0.52})
     with pytest.raises(ValidationError):
-        # wrong parametrs type
-        TransBase(model='Single Exponential Down', parameters=(175, 'cm^-1'))
+        # wrong parameters type
+        TransBase(model='Single Exponential Down',
+                  parameters=(175, 'cm^-1'))
+    with pytest.raises(ValidationError):
+        # wrong parameters type
+        TransBase(model='Single Exponential Down',
+                  parameters={'alpha0': (175, 'cm^-1'), 'T0': (300, 'K'), 'n': (0.52,)})
+    with pytest.raises(ValidationError):
+        # wrong parameters type
+        TransBase(model='Single Exponential Down',
+                  parameters={'alpha0': {'a': [175, 'cm^-1']}, 'T0': (300, 'K'), 'n': 0.52})
+    with pytest.raises(ValidationError):
+        # wrong parameters type
+        TransBase(model='Single Exponential Down',
+                  parameters={'alpha0': (175, 'cm^-1'), 'T0': 300, 'n': 0.52})
     with pytest.raises(ValidationError):
         # no alpha0
-        TransBase(model='Single Exponential Down', parameters={'T0': (300, 'K'), 'n': (0.52,)})
+        TransBase(model='Single Exponential Down',
+                  parameters={'T0': (300, 'K'), 'n': 0.52})
     with pytest.raises(ValidationError):
         # no T0
-        TransBase(model='Single Exponential Down', parameters={'alpha0': (175, 'cm^-1'), 'n': (0.52,)})
+        TransBase(model='Single Exponential Down',
+                  parameters={'alpha0': (175, 'cm^-1'), 'n': 0.52})
     with pytest.raises(ValidationError):
         # no n
-        TransBase(model='Single Exponential Down', parameters={'alpha0': (175, 'cm^-1'), 'T0': (300, 'K')})
+        TransBase(model='Single Exponential Down',
+                  parameters={'alpha0': (175, 'cm^-1'), 'T0': (300, 'K')})
     with pytest.raises(ValidationError):
-        # unsipported parameter key
-        TransBase(model='Single Exponential Down', parameters={'alpha0': (175, 'cm^-1'), 'T0': (300, 'K'),
-                                                               'n': (0.52,), 'unsupported': (175, 'cm^-1')})
+        # unsupported parameter key
+        TransBase(model='Single Exponential Down',
+                  parameters={'alpha0': (175, 'cm^-1'), 'T0': (300, 'K'), 'n': 0.52, 'unsupported': (175, 'cm^-1')})
     with pytest.raises(ValidationError):
         # non tuple value
-        TransBase(model='Single Exponential Down', parameters={'alpha0': (175, 'cm^-1'),
-                                                               'T0': (300, 'K'), 'n': 0.52})
+        TransBase(model='Single Exponential Down',
+                  parameters={'alpha0': (175, 'cm^-1'), 'T0': (300, 'K'), 'n': [0.52]})
     with pytest.raises(ValidationError):
         # long tuple
-        TransBase(model='Single Exponential Down', parameters={'alpha0': (175, 'cm^-1'),
-                                                               'T0': (300, 'K', 'J'), 'n': (0.52,)})
+        TransBase(model='Single Exponential Down',
+                  parameters={'alpha0': (175, 'cm^-1'), 'T0': (300, 'K', 'J'), 'n': 0.52})
     with pytest.raises(ValidationError):
         # non float first value
-        TransBase(model='Single Exponential Down', parameters={'alpha0': ('175', 'cm^-1'),
-                                                               'T0': (300, 'K'), 'n': (0.52,)})
+        TransBase(model='Single Exponential Down',
+                  parameters={'alpha0': ('hundred seventy five', 'cm^-1'), 'T0': (300, 'K'), 'n': 0.52})
     with pytest.raises(ValidationError):
         # non string second value
-        TransBase(model='Single Exponential Down', parameters={'alpha0': (175, 15),
-                                                               'T0': (300, 'K'), 'n': (0.52,)})
+        TransBase(model='Single Exponential Down',
+                  parameters={'alpha0': (175, {15: 4}), 'T0': (300, 'K'), 'n': 0.52})
     with pytest.raises(ValidationError):
         # units for n
-        TransBase(model='Single Exponential Down', parameters={'alpha0': (175, 'cm^-1'),
-                                                               'T0': (300, 'K'), 'n': (0.52, 'units')})
+        TransBase(model='Single Exponential Down',
+                  parameters={'alpha0': (175, 'cm^-1'), 'T0': (300, 'K'), 'n': (0.52, 'units')})
