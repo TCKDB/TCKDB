69be382d4a9875939a8bbbedb8412ecec57329bd
Alon Grinberg Dana <alongd@mit.edu>
2020-07-05 00:48:10 -0400

    Added path

--- tckdb/__init__.py
+++ tckdb/__init__.py
@@ -2,4 +2,5 @@
 Initialize the TCKDB module
 """
 
+from .path import tckdb_path
 from .version import VERSION
---
+++ tckdb/path.py
@@ -0,0 +1,8 @@
+"""
+TCKDB path module
+"""
+
+import os
+
+
+tckdb_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))  # absolute path to the TCKDB repo folder
