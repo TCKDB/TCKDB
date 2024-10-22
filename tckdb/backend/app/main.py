74f6a5497c6a407a74588aae567b8ca0a8bd55e8
OfirSimhi1612 <67858057+OfirSimhi1612@users.noreply.github.com>
2024-10-22 19:58:08 +0300

    run server

--- tckdb/backend/app/main.py
+++ tckdb/backend/app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from alembic.config import Config
 from alembic import command
+from tckdb.backend.app.core.config import ENV, FAST_API_PORT
+import uvicorn
 
 from tckdb.backend.app.api.api_v1.endpoints import species, bot, np_species, literature, batch
 
@@ -13,11 +15,25 @@ app = FastAPI(
 
 @app.on_event("startup")
 def on_startup():
-    alembic_config = Config("/code/tckdb/backend/alembic.ini")
-    command.upgrade(alembic_config, "head")
+    print(f'API is running on port {FAST_API_PORT}')
 
 app.include_router(species.router, prefix="/api/v1/species")
 app.include_router(np_species.router, prefix="/api/v1/np_species")
 app.include_router(bot.router, prefix="/api/v1/bot")
 app.include_router(literature.router, prefix="/api/v1/literature")
 app.include_router(batch.router, prefix="/api/v1/batch-upload")
+
+def main():
+    IS_DEV = ENV=='Development'
+    config = uvicorn.Config(
+                    "main:app", 
+                    port=int(FAST_API_PORT), 
+                    log_level= "info" if IS_DEV else "warn", 
+                    host="0.0.0.0", 
+                    reload=IS_DEV
+                )
+    server = uvicorn.Server(config)
+    server.run()
+
+if __name__ == "__main__":
+    main()
