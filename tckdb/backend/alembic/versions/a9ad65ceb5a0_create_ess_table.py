d311c87168a6236c93135d68f9037ce2b41a56b0
Calvin Pieters <calvinpieters@gmail.com>
2024-10-31 23:21:02 +0100

    Alembic
    Alembic environment file and version files added for creating database models

---
+++ tckdb/backend/alembic/versions/a9ad65ceb5a0_create_ess_table.py
@@ -0,0 +1,44 @@
+"""Create ess table
+
+Revision ID: a9ad65ceb5a0
+Revises: 97e84f0d0aa5
+Create Date: 2024-10-20 08:35:00.093855
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+from tckdb.backend.app.models.common import MsgpackExt
+
+# revision identifiers, used by Alembic.
+revision: str = "a9ad65ceb5a0"
+down_revision: Union[str, None] = "97e84f0d0aa5"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "ess",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("name", sa.String(length=100), nullable=False),
+        sa.Column("version", sa.String(length=100), nullable=False),
+        sa.Column("revision", sa.String(length=100), nullable=False),
+        sa.Column("url", sa.String(length=255), nullable=False),
+        sa.Column("reviewer_flags", MsgpackExt(), nullable=True),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_unique_constraint(
+        "ess_name_version_revision_uc", "ess", ["name", "version", "revision"]
+    )
+    op.create_index(op.f("ix_ess_id"), "ess", ["id"], unique=False)
+    pass
+
+
+def downgrade() -> None:
+    op.drop_index(op.f("ix_ess_id"), table_name="ess")
+    op.drop_table("ess")
+    pass
