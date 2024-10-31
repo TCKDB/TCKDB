8bc5a30e0d3c52a4cfab50aa0be539c206a88802
Calvin Pieters <calvinpieters@gmail.com>
2024-10-31 23:33:20 +0100

    Database Support base files
    Added AuditMixin Class
    Created a SoftDeleteQuery
    Updated Session

--- tckdb/backend/app/db/session.py
+++ tckdb/backend/app/db/session.py
@@ -6,10 +6,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 from tckdb.backend.app.core import config
-
+from tckdb.backend.app.db.query import SoftDeleteQuery
 
 engine = create_engine(config.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
-db_session = scoped_session(
-    sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# db_session = scoped_session(
+#     sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# )
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=engine, query_cls=SoftDeleteQuery
 )
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+db_session = scoped_session(SessionLocal)
+
+
+def get_db():
+    """
+    Dependency function to provide a SQLAlchemy database session
+
+    Yields:
+        Session: A SQLAlchemy database session object
+    """
+    db = db_session()
+    try:
+        yield db
+    finally:
+        db.close()
