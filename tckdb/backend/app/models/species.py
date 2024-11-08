6cc559d958ec8b834200861adcc9b3331e4c33dd
Calvin Pieters <calvinpieters@gmail.com>
2024-11-08 14:47:19 +0100

    PR Fix: Remove deleted_at column in species.py model

--- tckdb/backend/app/models/species.py
+++ tckdb/backend/app/models/species.py
@@ -525,7 +525,6 @@ class Species(Base, AuditMixin):
         DateTime(timezone=True), server_default=func.now(), nullable=False
     )
     retracted = Column(String(255), nullable=True)
-    deleted_at = Column(DateTime(timezone=True), nullable=True, default=None)
 
     # review
     reviewed = Column(Boolean, nullable=False, default=False)
