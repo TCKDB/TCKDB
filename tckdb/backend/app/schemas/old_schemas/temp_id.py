7bff60e6031a19e911fe1776ce446f143a97685a
Ofir Simhi <67858057+OfirSimhi1612@users.noreply.github.com>
2024-11-11 16:42:51 +0200

    delete old files

--- tckdb/backend/app/schemas/old_schemas/temp_id.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel, ConfigDict, Field
-
-
-class TempBase(BaseModel):
-    temp_id: str = Field(
-        ..., title="The temporary ID of the object for internal referencing"
-    )
-    model_config = ConfigDict(from_attributes=True, extra="forbid")
