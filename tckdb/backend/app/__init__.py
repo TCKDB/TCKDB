54a90d838763adeb0514a78724d31d20d3f1fbef
Ofir Simhi <67858057+OfirSimhi1612@users.noreply.github.com>
2024-11-11 16:57:04 +0200

    remove unused imports

--- tckdb/backend/app/__init__.py
+++ tckdb/backend/app/__init__.py
@@ -1,13 +1,3 @@
 """
 Initialize the TCKDB backend app module
 """
-
-import tckdb.backend.app.api.api_v1.endpoints
-import tckdb.backend.app.conversions
-import tckdb.backend.app.core
-import tckdb.backend.app.db
-import tckdb.backend.app.models
-
-# trunk-ignore(ruff/F401)
-# trunk-ignore(flake8/F401)
-import tckdb.backend.app.schemas
